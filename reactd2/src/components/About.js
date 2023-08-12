9e49f8d87ae152e52e545902be3da47d3fe344b7
M Shekhar Dora <63382577+Shekhar403@users.noreply.github.com>
2023-08-12 11:16:13 +0530

    Update About.js

--- reactd2/src/components/About.js
+++ reactd2/src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../styles/navbar.css"
 
 function About() {
   return (
     <div>
-        <h3 style={{color:"white"}}>Welcome to the chilling abyss of our horror website, where the veil between reality and the macabre is tantalizingly thin. Delve into a realm where shadows dance with sinister secrets and the unknown lurks in every corner of the digital domain. With each click, your heart races and your senses heighten, as tales of terror and spine-tingling narratives unfold before your eyes. From bone-chilling short stories to in-depth explorations of urban legends and supernatural phenomena, our horror website is a haven for those who seek the thrill of the unknown and the adrenaline of fear. But beware, for as you navigate its eerie pages, you may find yourself ensnared in a web of horror from which there is no escape. Will you brave the darkness and confront the horrors that await, or will you succumb to the haunting whispers that beckon you deeper into the abyss? The choice is yours, dear visitor, as you step into the realm of our horror website.</h3>
+        <h3 className='horror_text text-white'>Welcome to the chilling abyss of our horror website, where the veil between reality and the macabre is tantalizingly thin. Delve into a realm where shadows dance with sinister secrets and the unknown lurks in every corner of the digital domain. With each click, your heart races and your senses heighten, as tales of terror and spine-tingling narratives unfold before your eyes. From bone-chilling short stories to in-depth explorations of urban legends and supernatural phenomena, our horror website is a haven for those who seek the thrill of the unknown and the adrenaline of fear. But beware, for as you navigate its eerie pages, you may find yourself ensnared in a web of horror from which there is no escape. Will you brave the darkness and confront the horrors that await, or will you succumb to the haunting whispers that beckon you deeper into the abyss? The choice is yours, dear visitor, as you step into the realm of our horror website.</h3>
     </div>
   )
 }
